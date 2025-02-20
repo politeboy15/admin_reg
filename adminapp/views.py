9882c18c7548729b0435f778a4244dc05013a421
politeboy15 <muhammad14toxtasinov@gamil.com>
2025-02-20 21:27:33 +0500

    First changes and app

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+def login_required_decorator(func):
+    return login_required(func, login_url="login_page")
+
+
+@login_required_decorator
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')
+
+
+def login_page(request):
+    if request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(request, password=password, username=username)
+        if user is not None:
+            login(request, user)

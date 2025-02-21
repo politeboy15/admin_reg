a1ee5fb66650840dae19057418ce3ce96f87bdeb
politeboy15 <muhammad14toxtasinov@gamil.com>
2025-02-21 17:50:11 +0500

    Last update

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -5,16 +5,16 @@ from .models import *
 class FacultyForm(forms.ModelForm):
     class Meta:
         model = Faculty
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            "name":forms.TextInput(attrs={"class":"form-control"})
+            "name": forms.TextInput(attrs={'class': 'form-control'})
         }
 
 
 class KafedraForm(forms.ModelForm):
     class Meta:
         model = Kafedra
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            "name":forms.TextInput(attrs={"class":"form-control"})
+            "name": forms.TextInput(attrs={'class': 'form-control'})
         }
